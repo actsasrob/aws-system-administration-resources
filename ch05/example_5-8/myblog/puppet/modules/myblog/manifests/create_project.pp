187c76a33c95856f636d081c9c30cfb735337da8
actsasrob <acts.as.rob@gmail.com>
2017-04-17 17:14:50 -0400

    Adding example 5-8

---
+++ ch05/example_5-8/myblog/puppet/modules/myblog/manifests/create_project.pp
@@ -0,0 +1,17 @@
+class myblog::create_project {
+  # Create the Mezzanine project
+  exec { "init-mezzanine-project":
+    command => "/usr/local/bin/mezzanine-project $myblog::app_path",
+    user => "mezzanine",
+    creates => "$myblog::app_path/__init__.py",
+    notify => Exec["init-mezzanine-db"]
+  }
+
+  # Create the development SQLite database
+  exec { "init-mezzanine-db":
+    command => "/usr/bin/python manage.py createdb --noinput",
+    user => "mezzanine",
+    cwd => "$myblog::app_path",
+    refreshonly => true
+  }
+}
---
+++ ch05/example_5-8/myblog/puppet/modules/myblog/manifests/web.pp
@@ -0,0 +1,13 @@
+#include myblog::mynginx
+
+class myblog::web {
+  Class["myblog::web"] -> Class["myblog"]
+  require myblog::mynginx
+  supervisor::service { "myblog_web":
+    ensure => present,
+    enable => true,
+    command => "/usr/bin/python ${myblog::app_path}/manage.py runserver",
+    user => "mezzanine",
+    group => "mezzanine"
+  }
+}
