f509d67882aea2bdf3e93156e25a1a41516d793c
actsasrob <acts.as.rob@gmail.com>
2017-04-18 20:50:27 -0400

    Print ::app_path variable

--- ch05/example_5-8/myblog/puppet/modules/myblog/manifests/create_project.pp
+++ ch05/example_5-8/myblog/puppet/modules/myblog/manifests/create_project.pp
@@ -1,10 +1,14 @@
 class myblog::create_project {
+  notify { 'mezzanine_project_name':
+     message => "mezzine project name: ${myblog::app_path}",
+  }
+
   # Create the Mezzanine project
   exec { "init-mezzanine-project":
     command => "/usr/local/bin/mezzanine-project ${myblog::app_path}",
     user => "mezzanine",
     creates => "${myblog::app_path}/__init__.py",
-    notify => Exec["init-mezzanine-db"]
+    notify => Exec["init-mezzanine-db"],
   }
 
   # Create the development SQLite database
@@ -12,6 +16,6 @@ class myblog::create_project {
     command => "/usr/bin/python manage.py createdb --noinput",
     user => "mezzanine",
     cwd => $myblog::app_path,
-    refreshonly => true
+    refreshonly => true,
   }
 }
