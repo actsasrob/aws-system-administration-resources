860b56d61a2436caddf25e506271474f26c374cf
actsasrob <acts.as.rob@gmail.com>
2017-04-21 10:29:28 -0400

    Add puppet apply command reminder

--- ch05/example_5-8/install_files.sh
+++ ch05/example_5-8/install_files.sh
@@ -5,3 +5,5 @@ TARGET_DIR=/etc/puppetlabs/code/environments/production
 sudo mkdir -p $TARGET_DIR/modules/myblog/{manifests,modules}
 sudo cp -f myblog/puppet/manifests/*.pp $TARGET_DIR/manifests
 sudo cp -f myblog/puppet/modules/myblog/manifests/*.pp $TARGET_DIR/modules/myblog/manifests
+
+echo "Puppet apply command: sudo puppet apply ${TARGET_DIR}/manifests/site_notec2.pp"
