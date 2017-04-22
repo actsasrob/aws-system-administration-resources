136cf9ea0cf17229aae1cfddbe6ab011fa2ca384
actsasrob <acts.as.rob@gmail.com>
2017-04-22 14:26:24 -0400

    Adding example_5-9a temporarily

---
+++ ch05/example_5-9a/myblog/myblog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+aws cloudformation validate-template --template-body file://cloudformation/myblog.json
+if [ "$?" -ne 0 ]; then
+   echo "error: invalid cloudformation template. exiting..."
+   exit 1
+fi
+
+# ubuntu/images-testing/hvm-ssd/ubuntu-yakkety-daily-amd64-server-20170303.1 - ami-0f6fb419
+output=$(aws cloudformation create-stack --stack-name example-5-9-stack --template-body \
+file://cloudformation/myblog.json \
+--region us-east-1 --capabilities CAPABILITY_IAM \
+--parameters ParameterKey=AMI,ParameterValue=ami-0f6fb419 \
+ParameterKey=KeyName,ParameterValue=RobertHughes)
+
+if [ "$?" -eq 0 ]; then
+   stackid=$(echo "$output" | grep "StackId" | awk '{ print $2 }' | tr -d '"')
+   echo "info: waiting for stack creation to complete for stack ID ${stackid}..."
+   aws cloudformation wait stack-create-complete --stack-name $stackid
+else
+   echo "error: non-zero result returned from 'aws cloudformation' command"
+fi
+
