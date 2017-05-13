01735ddb433c027d892c2f36358b0f119138965f
actsasrob <acts.as.rob@gmail.com>
2017-05-13 15:35:10 -0400

    Add -y to apt invocations

--- ch05/example_5-1/setup_mezzanine.sh
+++ ch05/example_5-1/setup_mezzanine.sh
@@ -2,12 +2,16 @@
 
 set -x
 
-sudo apt install python-pip python-dev
-sudo apt istall libjpeg8 libjpeg8-dev libtiff5-dev zlib1g-dev libfreetype6-dev python-imaging
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+echo "\$MY_PATH=$MY_PATH"
+
+sudo apt -y install python-pip python-dev
+sudo apt -y istall libjpeg8 libjpeg8-dev libtiff5-dev zlib1g-dev libfreetype6-dev python-imaging
 
 sudo pip install Mezzanine
 sudo useradd mezzanine
-sudo mkdir /srv/myblog
+sudo mkdir -p /srv/myblog
 sudo chown mezzanine /srv/myblog
 
 sudo -u mezzanine mezzanine-project myblog /srv/myblog
@@ -17,13 +21,8 @@ cd /srv/myblog
 #sudo -u mezzanine python manage.py runserver 0.0.0.0:8000
 
 # Install/config Nginx
-sudo apt install nginx
+sudo apt -y install nginx
 sudo mv /etc/nginx/sites-enabled/default ~/default.orig
-
-MY_PATH=`dirname "$0"`
-MY_PATH=`( cd "$MY_PATH" && pwd )`
-
-echo "\$MY_PATH=$MY_PATH"
 
 sudo cp $MY_PATH/myblog.conf /etc/nginx/sites-enabled/myblog.conf
 sudo chmod 644 /etc/nginx/sites-enabled/myblog.conf
