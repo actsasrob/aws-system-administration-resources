16f6c3cc33984007ca046902a85b69c2db32cf8e
actsasrob <acts.as.rob@gmail.com>
2017-05-17 21:30:21 -0400

    Need to use sudo to copy files

--- ch05/example_5-21/myblog/install_files.sh
+++ ch05/example_5-21/myblog/install_files.sh
@@ -3,14 +3,14 @@
 TARGET_DIR=/etc/puppetlabs/code/environments/production
 
 echo "Installing required puppet modules..."
-sudo puppet module install puppetlabs-stdlib --version 4.16.0
-sudo puppet module install puppetlabs-concat --version 2.2.1
-sudo puppet module install ajcrowe-supervisord --version 0.6.1
-sudo puppet module install jfryman-nginx --version 0.3.0
+#sudo puppet module install puppetlabs-stdlib --version 4.16.0
+#sudo puppet module install puppetlabs-concat --version 2.2.1
+#sudo puppet module install ajcrowe-supervisord --version 0.6.1
+#sudo puppet module install jfryman-nginx --version 0.3.0
 
 echo "Installing custom modules/manifests..."
 sudo mkdir -p $TARGET_DIR/modules
-cp -r puppet/modules/* $TARGET_DIR/modules
+sudo cp -r puppet/modules/* $TARGET_DIR/modules
 #sudo mkdir -p $TARGET_DIR/modules/myblog/{manifests,modules,templates,files}
 sudo cp -f puppet/manifests/*.pp $TARGET_DIR/manifests
 #sudo cp -f puppet/modules/myblog/manifests/*.pp $TARGET_DIR/modules/myblog/manifests
